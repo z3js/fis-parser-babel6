fa00cccfa96064d59da3ae25ae9e6d037004d03d
lrenc <haojie.lrenc@gmail.com>
2016-10-12 14:18:49 +0800

    change es version

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 
 
 var babel        = require('babel-core');
-var preset2015   = require('babel-preset-es2015');
+var presetlatest = require('babel-preset-latest');
 var presetstage0 = require('babel-preset-stage-0');
-
-require("babel-polyfill");
 
 module.exports = function (content, file, conf) {
     conf = fis.util.extend({
        presets: [
-           preset2015,
+           presetlatest,
            presetstage0
+       ],
+       plugins: [
+           require("babel-polyfill")
        ]
    }, conf);
    return babel.transform(content, conf).code;

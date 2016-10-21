c706706ca938559c0118ec958a76b8a2bb61bd8d
lrenc <haojie.lrenc@gmail.com>
2016-10-21 10:45:38 +0800

    update

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ var presetlatest = require('babel-preset-latest');
 var presetstage0 = require('babel-preset-stage-0');
 
 module.exports = function (content, file, conf) {
+    if (!content) {
+        return content;
+    }
     conf = fis.util.extend({
        presets: [
            presetlatest,
            presetstage0
-       ],
+       ],   
        plugins: [
            require("babel-polyfill")
        ]
